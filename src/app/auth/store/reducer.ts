96d8b65e809f9172d9a554ded6a6bd03f6d92c1b
idrees <muhammad.idrees@gecurrent.com>
2023-01-18 18:09:58 +1100

    added auth reducer & import in auth module

---
+++ src/app/auth/store/reducer.ts
@@ -0,0 +1,20 @@
+import { Action, createReducer, on } from "@ngrx/store";
+import { AuthStateInterface } from "../types/authState.interface";
+import { register } from "./actions";
+
+const initialState: AuthStateInterface = {
+  isSubmitting: false,
+  idrees: true,
+};
+
+const authReducer = createReducer(
+  initialState,
+  on(
+    register,
+    (state): AuthStateInterface => ({ ...state, isSubmitting: true })
+  )
+);
+
+export function reducers(state: AuthStateInterface, action: Action) {
+  return authReducer(state, action);
+}
